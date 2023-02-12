bed25a76d8288d8e40a821c693117302a53cb579
Björn Sandmann <sandmann@codedata.solutions>
2023-02-12 13:52:46 +0100

    Added Serialization options

---
+++ Blocktrust.Common/Converter/SerializationOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace Blocktrust.Common.Converter;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public static class SerializationOptions
+{
+    public static JsonSerializerOptions UnsafeRelaxedEscaping = new()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        // TODO The default serialization options encode the + sign as \u002B. This is not compatible with Kotlin tests and implementation
+        // It can be considered as as security risk, but for now we will use the unsafe relaxed option
+        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+    
+    public static JsonSerializerOptions DisplaxIndented = new()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = true
+    };
+}
