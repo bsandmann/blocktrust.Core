6a3eccc03036deac128b47bc30c417c40301b458
Björn Sandmann <sandmann@codedata.solutions>
2023-02-19 09:47:14 +0100

    Moved different DidDoc related types to core

---
+++ Blocktrust.Common/Models/DidDoc/VerificationMaterial.cs
@@ -0,0 +1,13 @@
+﻿namespace Blocktrust.Common.Models.DidDoc;
+
+public struct VerificationMaterial
+{
+    public VerificationMaterial(VerificationMaterialFormat format, string value)
+    {
+        this.format = format;
+        this.value = value;
+    }
+    
+    public VerificationMaterialFormat format;
+    public string value; 
+}
---
+++ Blocktrust.Common/Models/Secrets/Secret.cs
@@ -0,0 +1,15 @@
+﻿namespace Blocktrust.Common.Models.Secrets;
+
+using Blocktrust.Common.Models.DidDoc;
+
+public class Secret {
+    public string Kid;
+    public VerificationMethodType Type;
+    public VerificationMaterial VerificationMaterial;
+
+    public Secret(string kid, VerificationMethodType type, VerificationMaterial verificationMaterial) {
+        this.Kid = kid;
+        this.Type = type;
+        this.VerificationMaterial = verificationMaterial;
+    }
+}
